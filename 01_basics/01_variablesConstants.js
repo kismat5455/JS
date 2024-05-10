e83cda6e8f02402c1196b1eba996114e40db88e1
KISMAT ALI <61203535+kismat5455@users.noreply.github.com>
2024-05-10 17:45:48 +0000

    Learn variables and constants

---
+++ 01_basics/01_variablesConstants.js
@@ -0,0 +1,9 @@
+const balance=1000;
+let accountEmail="aaaa@demo.com"
+var accountName="Demo"
+accountId=10000;
+let accountPlace
+
+// never use var due to block and function scope
+
+console.table([balance,accountEmail,accountName,accountId,accountPlace])
