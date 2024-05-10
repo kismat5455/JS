ef6972fd4b264605039436213ff90240ca41416d
KISMAT ALI <61203535+kismat5455@users.noreply.github.com>
2024-05-10 20:16:52 +0000

    ADD data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+let variable = "dsadasd"//string
+console.log(typeof (variable))
+
+variable=1223;//number
+console.log(typeof (variable))
+
+variable=true;//boolean
+console.log(typeof (variable))
+
+variable=null;//standalone value
+console.log(typeof (variable))
+
+
+
+//undefined
+
+//object
+
+//symbol->unique
+
+//note: null is an object while undefined is a standalone datatype
+
