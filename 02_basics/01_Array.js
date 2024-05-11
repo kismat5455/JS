6f841d1215435ad4835418310b3057d0e64dfd64
KISMAT ALI <61203535+kismat5455@users.noreply.github.com>
2024-05-11 23:59:38 +0000

    Add array content

---
+++ 02_basics/01_Array.js
@@ -0,0 +1,19 @@
+//Array
+
+let array1=new Array(1,2,3,4,5);
+
+let array2=[1,2,3,4,5];
+
+console.log(array1);
+console.log(array2);
+
+//some methods in array
+
+console.log(array2.concat(array1));
+console.log(...array1,...array2);
+
+console.log(array2.slice(1,3));
+
+
+
+//note there are many other methods
