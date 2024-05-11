f151d6139f0425895efe2710c2f42a59c562dc04
KISMAT ALI <61203535+kismat5455@users.noreply.github.com>
2024-05-11 15:58:40 +0000

    Add Math and Numbers

---
+++ 01_basics/06_Numbers_Maths.js
@@ -0,0 +1,15 @@
+//note: Number and Maths are also class having various methods that are useful for us.
+
+//Examples
+
+let number1= new Number(1000);
+
+console.log(number1);
+console.log(number1.toPrecision(5));
+
+
+
+console.log(Math.abs(-111));
+console.log(Math.sqrt(2));
+console.log(Math.round(2000.33));
+console.log(Math.max())
